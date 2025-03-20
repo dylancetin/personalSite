3e207b5c0608a3fd35b8f9146c440bdb38232918
Taytay <taylancetin06@gmail.com>
2025-03-20 14:54:12 +0300

    404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  output: "static",
+  site: "https://akordagency.com",
+  adapter: cloudflare(),
+  integrations: [tailwind(), solidJs()],
 });
+
