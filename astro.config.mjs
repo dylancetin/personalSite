1766b159d4588bd3d42448238921f4648213b553
Taytay <taylancetin06@gmail.com>
2025-08-02 20:40:30 +0300

    tw4 upgrade fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://akordagency.com",
+  site: "https://taylan.co",
   adapter: cloudflare(),
-  integrations: [tailwind(), solidJs()],
+  integrations: [solidJs()],
 });
-
